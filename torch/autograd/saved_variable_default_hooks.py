101fcde8bdec764969d7755d878583a451a53170
Victor Quach <quach@fb.com>
2021-08-03 08:19:18 -0700

    Add docstrings for save_on_cpu hooks
    This PR adds docstrings for CPU hooks introduced in #61928.
    
    Also uncomments the warning about pinned memory in CUDA semantics docs
    
    ghstack-source-id: c711d4ff378cb6096d9e454c9b5c89b66bbe44f9
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/62410

--- torch/autograd/saved_variable_default_hooks.py
+++ torch/autograd/saved_variable_default_hooks.py
@@ -8,6 +8,37 @@ def reset_saved_tensors_default_hooks():
     torch._C._autograd._reset_default_hooks()
 
 class save_on_cpu(object):
+    """"Context-manager under which tensors saved by the forward pass will be
+    stored on cpu, then retrieved for backward.
+
+    When performing operations within this context manager, intermediary
+    results saved in the graph during the forward pass will be moved to CPU,
+    then copied back to the original device when needed for the backward pass.
+    If the graph was already on CPU, no tensor copy is performed.
+
+    Use this context-manager to tradeoff speed for less GPU memory usage.
+
+    Args:
+        pin_memory (bool): If ``True`` tensors will be saved to CPU pinned memory
+                           during packing and copied to GPU asynchronously during unpacking.
+                           Defaults to ``False``.
+                           Also see :ref:`cuda-memory-pinning`.
+
+
+    Example::
+
+        >>> a = torch.randn(5, requires_grad=True, device="cuda")
+        >>> b = torch.randn(5, requires_grad=True, device="cuda")
+        >>> c = torch.randn(5, requires_grad=True, device="cuda")
+        >>> d = a * b # a and b are saved in the graph (on GPU)
+        >>> with torch.autograd.graph.save_on_cpu():
+        ...    e = d * c # d and c are saved on CPU
+        >>> f = a * e # a and e are saved on GPU
+        >>> del a, b, c, d, e
+        >>> # the content of a, b, e are still alive on GPU
+        >>> # the content of c and d only live on CPU
+
+    """
     def __init__(self, pin_memory=False):
         def pack_to_cpu(tensor):
             if not pin_memory:
